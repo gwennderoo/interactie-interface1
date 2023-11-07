e454d97da37740349e935544d287a7777f492739
gwennderoo <149670239+gwennderoo@users.noreply.github.com>
2023-11-07 09:51:50 +0100

    updated files

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,140 @@
 // JavaScript Document
 console.log("Howdy!");
+
+// MUNTBUTTON VAR 
+
+var muntButton1 = document.getElementById("coin1");
+var muntButton2 = document.getElementById("coin2");
+var muntButton3 = document.getElementById("coin3");
+var muntButton4 = document.getElementById("coin4");
+var muntButton5 = document.getElementById("coin5");
+var muntButton6 = document.getElementById("coin6");
+var muntButton7 = document.getElementById("coin7");
+var muntButton8 = document.getElementById("coin8");
+var muntButton9 = document.getElementById("coin9");
+var muntButton10 = document.getElementById("coin10");
+
+
+// TEKSTVAK VAR 
+
+var tekstVak1 = document.querySelector("main ol li:nth-of-type(1) section");
+var tekstVak2 = document.querySelector("main ol li:nth-of-type(2) section");
+var tekstVak3 = document.querySelector("main ol li:nth-of-type(3) section");
+var tekstVak4 = document.querySelector("main ol li:nth-of-type(4) section");
+var tekstVak5 = document.querySelector("main ol li:nth-of-type(5) section");
+var tekstVak6 = document.querySelector("main ol li:nth-of-type(6) section");
+var tekstVak7 = document.querySelector("main ol li:nth-of-type(7) section");
+var tekstVak8 = document.querySelector("main ol li:nth-of-type(8) section");
+var tekstVak9 = document.querySelector("main ol li:nth-of-type(9) section");
+var tekstVak10 = document.querySelector("main ol li:nth-of-type(10) section");
+
+
+
+
+// FUNCTION JAVA
+
+muntButton1.onclick = laatInfoZien
+
+function laatInfoZien() {
+    tekstVak1.classList.toggle("verschuiven");
+    console.log("kwak");
+
+};
+
+muntButton2.onclick = laatInfoZien2
+
+function laatInfoZien2() {
+    tekstVak2.classList.toggle("verschuiven");
+    console.log("kwak");
+
+};
+
+muntButton3.onclick = laatInfoZien3
+
+function laatInfoZien3() {
+    tekstVak3.classList.toggle("verschuiven");
+    console.log("kwak");
+
+};
+
+muntButton4.onclick = laatInfoZien4
+
+function laatInfoZien4() {
+    tekstVak4.classList.toggle("verschuiven");
+    console.log("kwak");
+
+};
+
+muntButton5.onclick = laatInfoZien5
+
+function laatInfoZien5() {
+    tekstVak5.classList.toggle("verschuiven");
+    console.log("kwak");
+
+};
+
+muntButton6.onclick = laatInfoZien6
+
+function laatInfoZien6() {
+    tekstVak6.classList.toggle("verschuiven");
+    console.log("kwak");
+
+};
+
+muntButton7.onclick = laatInfoZien7
+
+function laatInfoZien7() {
+    tekstVak7.classList.toggle("verschuiven");
+    console.log("kwak");
+
+};
+
+muntButton8.onclick = laatInfoZien8
+
+function laatInfoZien8() {
+    tekstVak8.classList.toggle("verschuiven");
+    console.log("kwak");
+
+};
+
+muntButton9.onclick = laatInfoZien9
+
+function laatInfoZien9() {
+    tekstVak9.classList.toggle("verschuiven");
+    console.log("kwak");
+
+};
+
+muntButton10.onclick = laatInfoZien10
+
+function laatInfoZien10() {
+    tekstVak10.classList.toggle("verschuiven");
+    console.log("kwak");
+
+};
+
+// coinButtons.forEach(coin =>  {
+//     coin.addEventListener('click', () => {
+//         alert('test');
+//         tekstVak.classList.toggle("verschuiven");
+
+
+// function tekstVakVerschijnt(){
+//     console.log("test")
+    // tekstVak.classList.toggle("verschuiven");
+// }
+
+
+// muntButton.onclick = tekstVakVerschijnt;
+
+
+// const coinButtons = document.querySelectorAll('.coin');
+//     });
+
+// });
+
+
+// dialog scrooge mcduck main
+var Scrooge = document.querySelector("dialog-Scrooge");
+
+

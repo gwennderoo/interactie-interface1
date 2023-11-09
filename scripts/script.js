3a0001a0d102874ec422799d91c5dfe665bf73d5
gwennderoo <149670239+gwennderoo@users.noreply.github.com>
2023-11-09 14:05:03 +0100

    Final site interface and interaction 2023

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,17 @@
+
+
+// ******************
 // JavaScript Document
+// ******************
+
+
 console.log("Howdy!");
 
-// MUNTBUTTON VAR 
+
+
+
+
+// MUNTBUTTON VAR TIJDSLIJN
 
 var muntButton1 = document.getElementById("coin1");
 var muntButton2 = document.getElementById("coin2");
@@ -15,7 +25,7 @@ var muntButton9 = document.getElementById("coin9");
 var muntButton10 = document.getElementById("coin10");
 
 
-// TEKSTVAK VAR 
+// TEKSTVAK VAR TIJDSLIJN
 
 var tekstVak1 = document.querySelector("main ol li:nth-of-type(1) section");
 var tekstVak2 = document.querySelector("main ol li:nth-of-type(2) section");
@@ -28,10 +38,10 @@ var tekstVak8 = document.querySelector("main ol li:nth-of-type(8) section");
 var tekstVak9 = document.querySelector("main ol li:nth-of-type(9) section");
 var tekstVak10 = document.querySelector("main ol li:nth-of-type(10) section");
 
+var audionaam3 = document.querySelector("audio:nth-of-type(3)")
 
 
-
-// FUNCTION JAVA
+// FUNCTION JAVA TIJDSLIJN
 
 muntButton1.onclick = laatInfoZien
 
@@ -100,8 +110,10 @@ function laatInfoZien8() {
 muntButton9.onclick = laatInfoZien9
 
 function laatInfoZien9() {
+
     tekstVak9.classList.toggle("verschuiven");
     console.log("kwak");
+    audionaam3.play()
 
 };
 
@@ -113,28 +125,69 @@ function laatInfoZien10() {
 
 };
 
-// coinButtons.forEach(coin =>  {
-//     coin.addEventListener('click', () => {
-//         alert('test');
-//         tekstVak.classList.toggle("verschuiven");
 
 
-// function tekstVakVerschijnt(){
-//     console.log("test")
-    // tekstVak.classList.toggle("verschuiven");
-// }
 
 
-// muntButton.onclick = tekstVakVerschijnt;
+
+// ********
+// DIALOOG 1
+
+var dialoog = document.getElementById("dialog-scrooge");
+var dialoogOpen = document.querySelector(".button-dialog");
+var dialoogSluit = document.querySelector(".sluiten");
+
+var audionaam = document.querySelector("audio:first-of-type")
+var audionaam2 = document.querySelector("audio:nth-of-type(2)")
+
+function openDialoog(){
+    
+    console.log("hoi")
+    dialoog.classList.add("zichtbaar");
+    audionaam.play()
+    
+}
+
+dialoogOpen.onclick = openDialoog;
+dialoogSluit.onclick = sluitDialoog;
+
+function sluitDialoog(){
+
+    dialoog.classList.remove("zichtbaar");
+    audionaam2.play()
+}
 
 
-// const coinButtons = document.querySelectorAll('.coin');
-//     });
-
-// });
 
 
-// dialog scrooge mcduck main
-var Scrooge = document.querySelector("dialog-Scrooge");
 
 
+// *********
+// DIALOOG 2
+
+var dialoog2 = document.getElementById("dialog-sleutel"); 
+// deze variable is voor de dialoog zelf
+
+var dialoogOpen2 = document.querySelector(".button-sleutel");
+// deze variable is voor de button om te openen
+
+var dialoogSluit2 = document.querySelector(".sluiten2");
+// deze variable is voor het x om de button dicht te doen
+
+
+function openDialoog2(){
+    
+    console.log("hoi")
+    dialoog2.classList.add("sleutel");
+    audionaam.play()
+    
+}
+
+dialoogOpen2.onclick = openDialoog2;
+dialoogSluit2.onclick = sluitDialoog2;
+
+function sluitDialoog2(){
+
+    dialoog2.classList.remove("sleutel");
+
+}
